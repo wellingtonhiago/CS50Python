fec7a96b05d01dd5019c2198e072e91e7003f93c
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-06-01 00:26:20 -0300

    final project 3 cs50 python

---
+++ fuel/fuel.py
@@ -0,0 +1,25 @@
+import math
+
+while True:
+
+    fuel = input("Fraction : ")
+    try :
+
+        numerator, denominator = fuel.split("/")
+        new_numerator = int(numerator)
+        new_denominator = int(denominator)
+        f = new_numerator / new_denominator
+
+        if f <= 1:
+            break
+
+    except (ValueError, ZeroDivisionError):
+        pass
+
+p = round(float(f * 100))
+if p <= 1:
+    print("E")
+elif p >= 99:
+    print ("F")
+else:
+    print(f"{p}%")
---
+++ taqueria/taqueria.py
@@ -0,0 +1,25 @@
+felipes = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+total = 0
+while True:
+    try:
+        item = input("Item: ").title()
+
+    except EOFError:
+        print()
+        break
+        
+    if item in felipes:
+        total += felipes[item]
+        print(f"Total: ${total:.2f}")
+
+
